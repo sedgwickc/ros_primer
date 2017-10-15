630d0b89f7c7fefa275acd8d58190c3b7c9fa00d
Charles <sedgwickcharles@gmail.com>
2017-10-14 22:25:01 -0600

    Create chllng_1 and chllng_2 nodes to demonstrate publishing and subscribing

---
+++ src/chllng_1.cpp
@@ -0,0 +1,110 @@
+#include "ros/ros.h"
+#include "std_msgs/Int32.h"
+
+#include <sstream>
+#include <string>
+
+std_msgs::Int32 num1;
+std_msgs::Int32ConstPtr num2;
+
+/**
+ * chllnge_1.cpp: a node that adds a two numbers. One number is entered by the
+ * and the other is retrieved from the adder topic.
+ *
+ * Alorithm:
+ * 1. ask the user for a number
+ *     2.1. publish the number to the adder topic
+ * 2. wait for the other node to publish the second number
+ * 3. display the addition of the two entered numbers in the form:
+ *          “{First Number} + {Second Number} = {Their Sum}”
+ * 3. exit
+ *
+ */
+int main(int argc, char **argv)
+{
+  /**
+   * The ros::init() function needs to see argc and argv so that it can perform
+   * any ROS arguments and name remapping that were provided at the command line.
+   * For programmatic remappings you can use a different version of init() which takes
+   * remappings directly, but for most command-line programs, passing argc and argv is
+   * the easiest way to do it.  The third argument to init() is the name of the node.
+   *
+   * You must call one of the versions of ros::init() before using any other
+   * part of the ROS system.
+   */
+  ros::init(argc, argv, "chllng_1");
+
+  /**
+   * NodeHandle is the main access point to communications with the ROS system.
+   * The first NodeHandle constructed will fully initialize this node, and the last
+   * NodeHandle destructed will close down the node.
+   */
+  ros::NodeHandle n;
+
+  /* retrieve a number from the user */
+  std::cout<<"Please enter the first number would like to add: "<<std::endl;
+  std::cin>>num1.data;
+
+  /* publish the number to the adder topic */
+
+  /**
+   * The advertise() function is how you tell ROS that you want to
+   * publish on a given topic name. This invokes a call to the ROS
+   * master node, which keeps a registry of who is publishing and who
+   * is subscribing. After this advertise() call is made, the master
+   * node will notify anyone who is trying to subscribe to this topic name,
+   * and they will in turn negotiate a peer-to-peer connection with this
+   * node.  advertise() returns a Publisher object which allows you to
+   * publish messages on that topic through a call to publish().  Once
+   * all copies of the returned Publisher object are destroyed, the topic
+   * will be automatically unadvertised.
+   *
+   * The second parameter to advertise() is the size of the message queue
+   * used for publishing messages.  If messages are published more quickly
+   * than we can send them, the number here specifies how many messages to
+   * buffer up before throwing some away.
+   */
+  ros::Publisher adder_pub = n.advertise<std_msgs::Int32>("adder", 1000);
+
+  ros::Rate loop_rate(10);
+
+  /**
+   * A count of how many messages we have sent. This is used to create
+   * a unique string for each message.
+   */
+  int count = 0;
+  while (ros::ok() && count < 10)
+  {
+    /**
+     * This is a message object. You stuff it with data, and then publish it.
+     */
+
+    /* store first number in message */
+    ROS_INFO("publishing <%d>", num1.data);
+
+    /**
+     * The publish() function is how you send messages. The parameter
+     * is the message object. The type of this object must agree with the type
+     * given as a template parameter to the advertise<>() call, as was done
+     * in the constructor above.
+     */
+    adder_pub.publish(num1);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+  }
+
+  /* subscribe to adder topic and wait until second number user wants to add is
+   * published */
+  std::string topic = "adder";
+  num2 = ros::topic::waitForMessage<std_msgs::Int32>(topic);
+
+  /* add numbers and output result */
+  int sum = num1.data + num2->data;
+  std::cout<<num1.data<<" + "<<num2->data<<" = "<<sum<<std::endl;
+
+
+  return 0;
+}
